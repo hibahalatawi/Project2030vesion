0e23c57253e329c73ee93d57020b221fa3ef08d0
amal <agalkhathami22@twq.idserve.net>
2023-05-23 11:50:18 +0300

    amal

---
+++ Project2030/DataModel.swift
@@ -0,0 +1,17 @@
+//
+//  DataModel.swift
+//  Project2030
+//
+//  Created by amalghurm on 03/11/1444 AH.
+//
+import SwiftUI
+
+struct malm: Identifiable {
+    var id = UUID ()
+    var title: String
+    var place: String
+    var image: String
+    var description: String
+    var Gimage: [String]
+    
+}
